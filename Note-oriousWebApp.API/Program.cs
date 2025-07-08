601c97f5dbddaecb553dd6de77bdbfab8b5df0a3
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-07-09 00:05:31 +0800

    fix: add users' repository and service

--- Note-oriousWebApp.API/Program.cs
+++ Note-oriousWebApp.API/Program.cs
@@ -2,20 +2,31 @@ using Microsoft.EntityFrameworkCore;
 using Note_oriousWebApp.API.Configurations;
 using Note_oriousWebApp.API.Repositories;
 using Note_oriousWebApp.API.Services;
+using Note_oriousWebApp.API.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<NotesRepository>();
 builder.Services.AddScoped<NotesService>();
+builder.Services.AddScoped<UsersRepository>();
+builder.Services.AddScoped<UsersService>();
 
 // Register DBContext with PostgreSQL
 builder.Services.AddDbContext<AppDBContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register JWTSettings and TokenHelper
+builder.Services.Configure<JWTSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.AddSingleton<TokenHelper>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Call Helpers
+string randomString = GenerateRandomStringHelper.GenerateRandomString(16);
+Console.WriteLine($"Generated Random String: {randomString}");
 
 var app = builder.Build();
 
@@ -27,6 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
