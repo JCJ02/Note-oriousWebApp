6c020d883073ab5a2121fdc25d1187d15bece1ff
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-07-14 21:27:40 +0800

    feat: create a data transfer object for creating and updating user

--- Note-oriousWebApp.API/DTOs/UsersDTOs/UpdateUserDTO.cs
+++ Note-oriousWebApp.API/DTOs/UsersDTOs/UpdateUserDTO.cs
@@ -1,10 +1,9 @@
 ﻿namespace Note_oriousWebApp.API.DTOs.UsersDTOs
 {
-    public class CreateUserDTO
+    public class UpdateUserDTO
     {
         public string Firstname { get; set; } = string.Empty;
         public string Lastname { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
     }
 }
