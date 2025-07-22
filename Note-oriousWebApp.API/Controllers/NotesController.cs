585da157d106ecfe3fc06c808e94f73095fcc588
jcj02 <jacobe.johncarlo.02022003@gmail.com>
2025-07-22 10:43:58 +0800

    feat: finished the CRUD operations for notes API

--- Note-oriousWebApp.API/Controllers/NotesController.cs
+++ Note-oriousWebApp.API/Controllers/NotesController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Note_oriousWebApp.API.DTOs.Notes;
+using Note_oriousWebApp.API.DTOs.NotesDTOs;
+using Note_oriousWebApp.API.DTOs.UsersDTOs;
+using Note_oriousWebApp.API.Helpers;
 using Note_oriousWebApp.API.Services;
 
 namespace note_oriouswebapp.api.controllers
 {
-    // Endpoint: api/notes
+    // Endpoint: api/Notes
     [ApiController]
     [Route("api/[controller]")]
     public class NotesController : ControllerBase
@@ -18,22 +21,23 @@ namespace note_oriouswebapp.api.controllers
             _notesService = notesService ?? throw new ArgumentNullException(nameof(notesService));
         }
 
-        // POST /api/notes
+        // CREATE a Note Method
+        // POST /api/Notes
         [HttpPost("{id}")]
-        public async Task<IActionResult> Create(int id, [FromBody] CreateNoteDTO note)
+        public async Task<IActionResult> Create(int id, [FromBody] CreateNoteDTO createNoteDTO)
         {
             try
             {
-                if (string.IsNullOrWhiteSpace(note.Title))
+                if (string.IsNullOrWhiteSpace(createNoteDTO.Title))
                 {
                     return BadRequest("Title is Required!");
                 }
-                else if (string.IsNullOrWhiteSpace(note.Content))
+                else if (string.IsNullOrWhiteSpace(createNoteDTO.Content))
                 {
                     return BadRequest("Content is Required!");
                 }
 
-                var create = await _notesService.Create(id, note);
+                var create = await _notesService.Create(id, createNoteDTO);
 
                 return CreatedAtAction(nameof(GetNoteByID), new { id = create.Id }, create);
             }
@@ -43,7 +47,8 @@ namespace note_oriouswebapp.api.controllers
             }
         }
 
-        // GET /api/notes
+        // GET Notes Method
+        // GET /api/Notes
         [HttpGet]
         public async Task<IActionResult> GetNotes()
         {
@@ -60,7 +65,8 @@ namespace note_oriouswebapp.api.controllers
             }
         }
 
-        // GET /api/notes/{id}
+        // GET a Note Method
+        // GET /api/Notes/{id}
         [HttpGet("{id}")]
         public async Task<IActionResult> GetNoteByID(int id)
         {
@@ -78,6 +84,48 @@ namespace note_oriouswebapp.api.controllers
             }
         }
 
+        // UPDATE a Note Method
+        // PUT /api/Notes/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateNoteDTO updateNoteDTO)
+        {
+            try
+            {
+
+                if (string.IsNullOrWhiteSpace(updateNoteDTO.Title))
+                {
+                    return BadRequest("Title is Required!");
+                }
+                else if (string.IsNullOrWhiteSpace(updateNoteDTO.Content))
+                {
+                    return BadRequest("Content is Required");
+                }
+
+                var updatedNote = await _notesService.Update(id, updateNoteDTO);
+                return Ok(updateNoteDTO);
+
+            }
+            catch (Exception error)
+            {
+                return StatusCode(500, error.Message);
+            }
+        }
+
+        // SOFT-DELETE a Note Method
+        // DELETE /api/Notes/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> SoftDelete(int id, [FromBody] SoftDeleteNoteDTO softDeleteNoteDTO)
+        {
+            try
+            {
+                var softDeletedUser = await _notesService.SoftDelete(id, softDeleteNoteDTO);
+                return Ok($"Note {id} is now Deleted at {softDeletedUser.DeletedAt}.");
+            }
+            catch (Exception error)
+            {
+                return StatusCode(500, error.Message);
+            }
+        }
 
     }
 }
